7a155e2a0b1e666190b8960ba3c15825a0c2a3ef
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-07 09:41:25 +0300

    add homeController

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import homeController from './controllers/homeController.js';
 
 const app = express();
 
 app.engine('hbs', handlebars.engine({
-    extname : '.hbs',
+    extname : 'hbs',
 }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
 app.use(express.static('public')); 
 
-app.get('/', (req, res) => {
-    res.render('home'); 
-});
+app.use(homeController);
+
+
 
 
 

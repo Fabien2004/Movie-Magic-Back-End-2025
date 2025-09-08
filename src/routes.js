4a6abb43be38e17e7b8cbfef9ceb4ccb488c1ea4
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-08 14:07:45 +0300

    add 404 page

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,17 @@ import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
 
+
 const router = Router();
  
 router.use(homeController);
-router.use('/movies', movieController)
+router.use('/movies', movieController);
+
+router.use((req, res) => {
+  res.status(404).render('404')
+})
+
+ 
+
 
 export default router;  

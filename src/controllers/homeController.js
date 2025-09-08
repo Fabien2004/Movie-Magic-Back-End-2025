741e47694e4c49b6967c6084b13eba4e1a886ced
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-08 09:36:53 +0300

    add movie services and data

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,16 @@
 import { Router } from 'express';
 import movieService from '../services/movieService.js';
 
+
 const router = Router();
 
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+     const movies = await movieService.getAll();
     res.render('home', { movies });
 });
 
-router.get('/about', (req, res) => { 
-    const movies = movieService.getAll();
+router.get('/about', async (req, res) => { 
     res.render('home/about');
 });
 

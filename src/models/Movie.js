451bea5efc86c6c1c1e80fd4f696a8d81be79460
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-11 14:01:41 +0300

    add movie model

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,1 +1,16 @@
-   
+import { Schema } from 'mongoose';
+import { generatePath } from 'react-router';
+
+const movieSchema = new Schema({
+    title: { type: String, required: true },
+    director: { type: String, required: true },
+    genre : { type: String, required: true },
+    rating: { type: Number, min: 0, max: 10 },
+    year: { type: Number, min: 1888 }, 
+    description : { type: String },
+    imageUrl: { type: String }
+
+});
+const Movie = model('Movie', movieSchema);
+
+export default Movie;

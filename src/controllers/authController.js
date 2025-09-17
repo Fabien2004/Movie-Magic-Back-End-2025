5a7910fcd186e5ace99bd7b381481602bceda443
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-17 12:33:25 +0300

    user creation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import authServeice from '../services/authService.js';
 
 const router = Router();
 
+
 router.get('/register', (req, res) => {
     res.render('auth/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { email, password, rePassword } = req.body;
+    await authServeice.register(email, password);
+    res.redirect('/auth/login');
 });
 
 export default router;

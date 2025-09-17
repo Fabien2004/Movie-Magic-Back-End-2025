e2d26fd0a358eb1e5e277fbe237c854be2db30bd
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-17 13:43:20 +0300

    validate login password and email

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,23 @@
 import User from '../models/User.js';
-
+import bcrypt from 'bcrypt.';
 const register = (email, password) => {
     return User.create({ email, password });
 };
-const login = (email, password) => {
-    return User.find({ email, password });
+const login = async  (email, password) => {
+    const user = await User.findOne({ email})
+    if(!user){
+        throw new error('Invalid email or password');
+    }
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if(!isValid){
+        throw new error('Invalid password');
+    }
+    
+   
+    
+
+    
 };
 
 

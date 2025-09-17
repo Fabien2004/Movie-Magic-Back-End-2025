b1f06eb46231272ea5c464ca7ac918b8f8a65def
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-17 13:50:57 +0300

    generate jwt token with login logic

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,23 +1,29 @@
 import User from '../models/User.js';
-import bcrypt from 'bcrypt.';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = 'urbv948t7g5vcuni324h57vhucnd942vr32';
+
 const register = (email, password) => {
     return User.create({ email, password });
 };
-const login = async  (email, password) => {
-    const user = await User.findOne({ email})
-    if(!user){
-        throw new error('Invalid email or password');
+const login = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new Error('Invalid email or password');
     }
     const isValid = await bcrypt.compare(password, user.password);
 
-    if(!isValid){
-        throw new error('Invalid password');
+    if (!isValid) {
+        throw new Error('Invalid password');
     }
-    
-   
-    
 
-    
+    const payload = {
+        _id: user._id,
+    }
+    const token = jwt.sign(payload, SECRET, { expiresIn: '2d' });
+
+    return token
 };
 
 

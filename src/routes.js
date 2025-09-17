6e0660ddf35f422e78f3ca7502fe20609af57c3c
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-17 11:18:25 +0300

    add register page

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
 import castController from './controllers/castController.js';
+import authController from './controllers/authController.js';
 
 const router = Router();
 
 router.use(homeController);
 router.use('/movies', movieController);
 router.use('/casts', castController);
+router.use('/auth', authController);
 
 router.use((req, res) => {
   res.status(404).render("404"); 

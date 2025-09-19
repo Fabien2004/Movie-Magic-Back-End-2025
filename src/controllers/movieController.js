c0c5eeddf6c8edd507bcf914534c290b0e6d2407
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-09-19 17:41:22 +0300

    add route guard

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -2,15 +2,17 @@ import { Router } from "express";
 
 import movieService from "../services/movieService.js";
 import castService from "../services/castService.js";
+import { isAuth } from "../middlewares/authMiddleware.js"
+
+
 
 const router = Router();
 
-// URL: /movies/create
-router.get('/create', (req, res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('movies/create');
 });
 
-router.post('/create', async (req, res) => {
+router.post('/create', isAuth, async (req, res) => {
     const movieData = req.body;
     const ownerId = req.user?._id;
     await movieService.create(movieData, ownerId);
@@ -28,21 +30,21 @@ router.get('/search', async (req, res) => {
 router.get('/:movieId/details', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean(); 
-    const isOwner = req.user?._id == movie.owner;
+    const isOwner = movie.owner && movie.owner.toString() === req.user?._id;
 
     
 
     res.render('movies/details', { movie, isOwner});
 });
 
-router.get('/:movieId/attach', async (req, res) => {
+router.get('/:movieId/attach', isAuth, async (req, res) => {
     const movie = await movieService.getOne(req.params.movieId).lean();
     const casts = await castService.getAllWithout(movie.casts).lean();
 
     res.render('movies/attach', { movie, casts });
 });
 
-router.post('/:movieId/attach', async (req, res) => {
+router.post('/:movieId/attach', isAuth, async (req, res) => {
     const movieId = req.params.movieId;
     const castId = req.body.cast;
     const character = req.body.character;
@@ -52,19 +54,19 @@ router.post('/:movieId/attach', async (req, res) => {
     res.redirect(`/movies/${movieId}/details`);
 });
 
-router.get('/:movieId/delete', async (req,res) => {
+router.get('/:movieId/delete', isAuth, async (req,res) => {
    const movieId = req.params.movieId;
    await movieService.remove(movieId);
    res.redirect('/');
 });
 
-router.get('/:movieId/edit', async (req,res) => {
+router.get('/:movieId/edit', isAuth, async (req,res) => {
    const movieId = req.params.movieId;
    const movie = await movieService.getOne(movieId).lean();
    res.render('movies/edit', { movie })
 });
 
-router.post('/:movieId/edit', async (req,res) => {
+router.post('/:movieId/edit', isAuth, async (req,res) => {
     const movieData = req.body;
    const movieId = req.params.movieId;
    await movieService.edit(movieId, movieData);
--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,29 +1,38 @@
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../config/constant.js'
 
- export const authMiddleware = (req, res, next) => {
+export const authMiddleware = (req, res, next) => {
     const token = req.cookies['auth'];
 
-    if(!token){
-        return next(); 
+    if (!token) {
+        return next();
     }
 
     try {
-      const decodedToken = jwt.verify(token, JWT_SECRET);
+        const decodedToken = jwt.verify(token, JWT_SECRET);
         const user = {
-            _id : decodedToken._id,
-            email : decodedToken.email, 
+            _id: decodedToken._id,
+            email: decodedToken.email,
         };
-         req.user = user;
-         res.locals.userId = user._id;
-         res.locals.userEmail = user.email;
-         res.locals.isAuthenticated = true;
+        req.user = user;
+        req.isAuthenticated = true;
+        res.locals.userId = user._id;
+        res.locals.userEmail = user.email;
+        res.locals.isAuthenticated = true;
 
         return next();
     } catch (err) {
         res.clearCookie('auth');
-       res.redirect('/auth/login');
-    };
-
-
+        res.redirect('/auth/login');
+    }
 };
+export const isAuth = (req, res, next) => {
+    if (!req.isAuthenticated) {
+        return res.redirect('/auth/login');
+    }
+    return next();
+}
+
+
+
+
